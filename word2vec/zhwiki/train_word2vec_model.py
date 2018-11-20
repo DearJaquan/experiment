6bacac80c96ec6a57c39bd3b4f99d3638a831a75
756462516@qq.com <756462516@qq.com>
2018-11-20 14:49:32 +0800

    训练模型

--- word2vec/zhwiki/train_word2vec_model.py
+++ word2vec/zhwiki/train_word2vec_model.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
-# train_word2vec_model.py 用于训练模型
-
+# step3：train_word2vec_model.py 用于训练模型
+'''script parameters：
+C:\work\experiment\word2vec\zhwiki\corpus\wiki.zh.text.seg C:\work\experiment\word2vec\zhwiki\word2vec_model\wiki.zh.text.model
+C:\work\experiment\word2vec\zhwiki\word2vec_model\wiki.zh.text.vector
+'''
 import logging
 import os.path
 import sys
 import multiprocessing
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 
 from gensim.corpora import WikiCorpus
 from gensim.models import Word2Vec
@@ -22,11 +27,12 @@ if __name__=='__main__':
     if len(sys.argv) < 4:
         print(globals()['__doc__'] % locals())
         sys.exit(1)
-    inp, outp1, outp2 = sys.argv[1:4]
+    inp, outp1, outp2 = sys.argv[1:4]   #argv[0]默认是系统参数，自定义参数从argv[1]开始
 
     model = Word2Vec(LineSentence(inp), size=400, window=5, min_count=5, workers=multiprocessing.cpu_count())
 
     # trim unneeded model memory = use(much) less RAM
     # model.init_sims(replace=True)
-    model.save(outp1)
-    model.save_word2vec_format(outp2, binary=False)
+    model.save(outp1)   #   输出文件默认为ANSI编码
+    # model.save_word2vec_format(outp2, binary=False)   #会报错DeprecationWarning: Deprecated. Use model.wv.save_word2vec_format instead.
+    model.wv.save_word2vec_format(outp2, binary=False)  #   输出文件默认为utf-8编码
